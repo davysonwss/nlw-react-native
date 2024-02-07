fca7a1068380652f5dc69446adb9e55d0abf5fe0
Davyson <davysonwss@hotmail.com>
2024-02-07 16:34:55 -0300

    :tada: First commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function Home() {
+  return (
+    <View className='bg-slate-900'>
+      <Text className="text-red-50">Hello, World!</Text>
+    </View>
+  )
+}
